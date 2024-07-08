3fe92e0659df995b721d232c9bfda549cf52b922
Adrian Reapor <diatum19@gmail.com>
2024-07-08 15:11:58 +0800

    base html in redone in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,54 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Menu from "./components/Menu";
+import Modal from "./components/Modal";
 
 export default function App() {
-  return <div>My App</div>;
+  const showModal = false;
+
+  return (
+    <>
+      <main>
+        <div className="grid" data-id="grid">
+          <div className="turn" data-id="turn">
+            <span className="material-symbols-outlined yellow"> close </span>
+            <p className="yellow">Player 1, you're up!</p>
+          </div>
+
+          <Menu onAction={(action) => console.log(action)} />
+
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((squareId) => {
+            return (
+              <div key={squareId} className="square shadow">
+                <span className="material-symbols-outlined yellow">close</span>
+              </div>
+            );
+          })}
+
+          <div
+            className="score shadow"
+            style={{ backgroundColor: "var(--yellow)" }}
+          >
+            <p>Player 1</p>
+            <span data-id="p1-wins">0 Wins</span>
+          </div>
+          <div className="score shadow">
+            <p>Ties</p>
+            <span data-id="ties">0</span>
+          </div>
+          <div
+            className="score shadow"
+            style={{ backgroundColor: "var(--turquoise)" }}
+          >
+            <p>Player 2</p>
+            <span data-id="p2-wins">0 Wins</span>
+          </div>
+        </div>
+      </main>
+
+      <Footer />
+
+      {showModal && <Modal message="Player 1 wins!" />}
+    </>
+  );
 }
---
+++ src/components/Menu.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import "./Menu.css";
+
+type Props = {
+  onAction(action: "reset" | "new-round"): void;
+};
+
+export default function Menu({ onAction }: Props) {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <div className="menu">
+      <button
+        className="menu-btn turquoise shadow"
+        onClick={() => setMenuOpen((prev) => !prev)}
+      >
+        Actions
+        <span className="material-symbols-outlined">
+          {menuOpen ? "keyboard_arrow_up" : "keyboard_arrow_down"}
+        </span>
+      </button>
+      {menuOpen && (
+        <div className="items border shadow">
+          <button onClick={() => onAction("reset")}>Reset</button>
+          <button onClick={() => onAction("new-round")}>New Round</button>
+        </div>
+      )}
+    </div>
+  );
+}
